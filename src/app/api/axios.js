0897ad960c3e87a398d20b07258b70b0a2932cef
didier-snake <didier.djakoua@gmail.com>
2024-05-07 14:12:18 +0100

    users sales list

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://158.220.98.45:5000",
+  baseURL: "http://158.220.98.45:5500",
   withCredentials: true,
 });
 export default api;
