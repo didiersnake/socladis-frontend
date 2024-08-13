4629f508726e3a02f6c0c10983525c7e95a8aecc
didier-snake <didier.djakoua@gmail.com>
2024-08-13 11:44:36 +0100

    ..

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,7 @@
 export const formatDate = (antDate) => {
   const date = new Date(antDate);
   const month = date.getMonth() + 1;
-  // const day = date.getDay() + 1;
-  const formattedDate = `${date.getDay()}-${month < 10 ? "0" + month : month}-${date.getFullYear()}`;
+  const day = date.getDate();
+  const formattedDate = `${day < 10 ? "0" + day : day}-${month < 10 ? "0" + month : month}-${date.getFullYear()}`;
   return formattedDate;
 };
